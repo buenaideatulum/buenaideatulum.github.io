db12a993f835c3dba9d848cebc4d944d039f9573
Diego Gonzalez <51349342+buenaideatulum@users.noreply.github.com>
2024-09-18 18:14:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// Registro de usuario
-document.getElementById('register-btn').addEventListener('click', () => {
-    const email = document.getElementById('register-email').value;
-    const password = document.getElementById('register-password').value;
-
-    createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-        // Registro exitoso
-        const user = userCredential.user;
-        alert('Registro exitoso. Ahora puedes iniciar sesión.');
-        console.log('Usuario registrado:', user.email);
-    })
-    .catch((error) => {
-        alert('Error al registrarse: ' + error.message);
-        console.error('Error al registrarse:', error);
-    });
-});
-
 // Inicio de sesión
 document.getElementById('login-btn').addEventListener('click', async () => {
     const email = document.getElementById('login-email').value;
@@ -24,19 +6,25 @@ document.getElementById('login-btn').addEventListener('click', async () => {
     // Comprueba si los campos no están vacíos
     if (email === '' || password === '') {
         alert('Por favor, llena todos los campos.');
+        console.log('Campos de correo o contraseña vacíos');
         return;
     }
+
+    console.log('Intentando iniciar sesión con', email);
 
     signInWithEmailAndPassword(auth, email, password)
     .then(async (userCredential) => {
         // Inicio de sesión exitoso
         const user = userCredential.user;
+        console.log('Inicio de sesión exitoso', user);
+
+        // Oculta la pantalla de autenticación y muestra la pantalla de la app
         document.getElementById('auth').style.display = 'none';
         document.getElementById('app').style.display = 'block';
         document.getElementById('user-name').textContent = user.email;
 
         // Mensaje de depuración
-        console.log('Inicio de sesión exitoso:', user.email);
+        console.log('Pantalla de Autenticación oculta y Pantalla de Escaneo mostrada.');
 
         score = await getUserScore(user.uid);
         scoreDisplay.textContent = score;
@@ -44,7 +32,8 @@ document.getElementById('login-btn').addEventListener('click', async () => {
         startCamera();
     })
     .catch((error) => {
+        // Manejo de errores
         alert('Error al iniciar sesión: ' + error.message);
-        console.error('Error al iniciar sesión:', error);
+        console.error('Error al iniciar sesión:', error.code, error.message);
     });
 });
